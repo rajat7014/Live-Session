51933bd6b9702eb5cc9fe7a7a8026f3ff405ca33
rajat7014 <132184024+rajat7014@users.noreply.github.com>
2025-11-08 09:35:20 +0530

    initial commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.prepare().then(() => {
     socket.on('disconnect', () => console.log('🔴 Client disconnected'))
   })
 
-  const port = 3000
+  const port = 1000
   server.listen(port, () =>
     console.log(`✅ Server running on http://localhost:${port}`)
   )
